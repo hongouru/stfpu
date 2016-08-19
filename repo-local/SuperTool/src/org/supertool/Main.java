55bac7e17f0eaf184488995cdd2499a7ae8bd4da
hongouru <hongouru@hotmail.com>
2016-08-19 11:58:28 -0300

    LE PUSE UN PRINTLN NUEVO

--- repo-local/SuperTool/src/org/supertool/Main.java
+++ repo-local/SuperTool/src/org/supertool/Main.java
@@ -1,10 +1,14 @@
+/* AUTOR: Mauri 
+ * Company: SoretSoft 2016
+ */
+
 package org.supertool;
 
 public class Main {
 
 	public static void main(String[] args) {
 		System.out.println("hola mundo supertool!!!");
-
+		System.out.println("Hey ho ho");
 	}
 
 }
