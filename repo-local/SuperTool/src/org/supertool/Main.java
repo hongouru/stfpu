40249ef11d61ec08b05ea7e44c5ef61dc300a488
hongouru <hongouru@hotmail.com>
2016-08-19 13:27:49 -0300

    Mensaje adaptado

--- repo-local/SuperTool/src/org/supertool/Main.java
+++ repo-local/SuperTool/src/org/supertool/Main.java
@@ -7,8 +7,8 @@ package org.supertool;
 public class Main {
 
 	public static void main(String[] args) {
-		System.out.println("hola mundo supertool!!!");
-		System.out.println("Hey ho ho.");
+		System.out.println("Este es el PERSONALUSE");
+		System.out.println("SI SI QUE BIEN");
 	}
 
 }
